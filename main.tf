8451198f08dc081b4249b68b73d8552b20b22d69
Sreeram K R <krsreeram007@gmail.com>
2026-06-23 22:25:53 +0530

    update Debian OS version in main.tf and refine professional summary, experience details, and project highlights in resume.tex

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "debian-cloud/debian-13"
       type  = "pd-standard"
       size  = 20
     }
