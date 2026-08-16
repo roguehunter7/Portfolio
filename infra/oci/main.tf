594a755a97aacec9398a1812a18142b23414ed8f
roguehunter7 <krsreeram007@gmail.com>
2026-08-16 14:34:26 +0530

    fix: use remote-managed cloudflared tunnel pattern (service install <token>); verify registration before sentinel

--- infra/oci/main.tf
+++ infra/oci/main.tf
@@ -117,7 +117,6 @@ resource "oci_core_instance" "portfolio_node" {
     ssh_authorized_keys = var.ssh_public_key
     user_data = base64encode(templatefile("${path.module}/cloud-init.yaml.tftpl", {
       cloudflare_tunnel_token = var.cloudflare_tunnel_token
-      ssh_hostname            = var.ssh_hostname
     }))
   }
 
